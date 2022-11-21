7072afc1dac1fca8a8fdddfcf3d0e8d2222945cf
Grzegorz <grzegorzboguslaw@gmail.com>
2022-11-21 19:17:19 +0100

    added module ceaser

---
+++ lab7/caesar.py
@@ -0,0 +1,14 @@
+
+ALPHABET_SIZE = 26
+ALPHABET_FIRST = ord('A')
+
+def encrypt_caesar(input : str, key : int) -> str:
+    result = ""
+    for letter in input:
+        value = (ord(letter) - ALPHABET_FIRST + key) % ALPHABET_SIZE
+        result += chr(ALPHABET_FIRST + value)
+
+    return result
+
+def decrypt_caesar(input : str, key : int) -> str:
+    return encrypt_caesar(input, ALPHABET_SIZE - key)
